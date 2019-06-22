16e47d5982bdc379aee54f631ef4ba33e8e375fc
Robert Ridley <robert.n.ridley@gmail.com>
2019-06-23 02:53:35 +0800

    allow cors

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
